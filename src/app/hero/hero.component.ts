3571fd9df9520e9de5ab45befe3262e3242514ef
Dineo Mathibela <dineomathibela161@gmail.com>
2023-11-19 00:56:39 +0200

    add: role function init

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
 @Component({
@@ -6,8 +6,66 @@ import { Title } from '@angular/platform-browser';
   templateUrl: './hero.component.html',
   styleUrls: ['./hero.component.css']
 })
-export class HeroComponent {
-  constructor(private TitleService: Title) {
-    this.TitleService.setTitle("Ms Mathibela - Home")
+export class HeroComponent implements OnInit {
+  constructor(private titleService: Title) {
+    this.titleService.setTitle("Ms Mathibela - Home");
+  }
+
+  roles = ['Backend Developer', 'Frontend Developer'];
+  currentRoleIndex = 0;
+  typingSpeed = 150;
+  textElement: HTMLElement | null = null;
+  typingInterval: any;
+
+  ngOnInit(): void {
+    this.textElement = document.getElementById('developerRole');
+
+    if (this.textElement) {
+      this.typeNextRole();
+    }
+  }
+
+  typeNextRole(): void {
+    if (this.textElement) {
+      const currentRole = this.roles[this.currentRoleIndex];
+      const characters = currentRole.split('');
+
+      let i = 0;
+      this.typingInterval = setInterval(() => {
+        if (i < characters.length) {
+          this.textElement!.textContent += characters[i];
+          i++;
+        } else {
+          clearInterval(this.typingInterval);
+          setTimeout(() => {
+            this.deleteCurrentRole();
+          }, 1500);
+        }
+      }, this.typingSpeed);
+    }
+  }
+
+  deleteCurrentRole(): void {
+    if (this.textElement) {
+      const currentText = this.textElement.textContent!;
+      let i = currentText.length - 1;
+
+      const interval = setInterval(() => {
+        if (i >= 0) {
+          this.textElement!.textContent = currentText.slice(0, i);
+          i--;
+        } else {
+          clearInterval(interval);
+          this.currentRoleIndex = (this.currentRoleIndex + 1) % this.roles.length;
+          setTimeout(() => {
+            this.typeNextRole();
+          }, 1500);
+        }
+      }, this.typingSpeed);
+    }
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.typingInterval);
   }
 }
