39752f200e6c451fc78647b947988a0a63c63758
Dineo Mathibela <dineomathibela161@gmail.com>
2023-11-18 23:44:24 +0200

    init: contact, hero, skills, projects, and about

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { HeroComponent } from './hero/hero.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SkillsComponent } from './skills/skills.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    HeroComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
